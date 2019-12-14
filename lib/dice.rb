d3dff4f8bb3eff171ac691410a4226f28a86e6f4
ncaudill27 <nelson.a.caudill@gmail.com>
2019-12-14 17:11:51 -0500

    Prep dice.rb for easier spec writing

--- lib/dice.rb
+++ lib/dice.rb
@@ -2,10 +2,12 @@ class Dice
   
   @@list = []
 
-  def initialize(dice_sides, dice_ammount=1) 
-    @sides = dice_sides 
-    @ammount = dice_ammount
-    @name = "#{dice_ammount}d#{dice_sides}" #ex. '2d4'
+  attr_accessor :sides, :count, :name
+  
+  def initialize(sides, count=1) 
+    @sides = sides 
+    @count = count
+    @name = "#{count}d#{sides}" #ex. '2d4'
     @@list << self
   end
    
@@ -14,7 +16,7 @@ class Dice
   end
 
   def roll
-    total = @ammount * @sides 
+    total = @count * @sides 
     rand(1..total)
   end
 
@@ -22,5 +24,5 @@ class Dice
     roll4d6 = [rand(1..6), rand(1..6),  rand(1..6), rand(1..6)]
     roll4d6.sum - roll4d6.min # Discard lowest roll
   end
-
+  
 end
