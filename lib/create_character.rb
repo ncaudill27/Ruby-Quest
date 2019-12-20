0ec447bf6afb7c3bd0d3b10226e1bbb5dde0bad3
ncaudill27 <nelson.a.caudill@gmail.com>
2019-12-19 20:21:01 -0500

    Roughly lay out CreateCharacter spec

--- lib/create_character.rb
+++ lib/create_character.rb
@@ -1,46 +1,62 @@
 class CreateCharacter 
  
-  attr_accessor :name, :race, :class
+  attr_accessor :character
   
   def initializer
+    @character = Character.new
     self.get_name
     self.get_race
     self.get_class
+    self.start_stats
     #Im still trying to figure out how to
     #create an object from the character class
     #whithin a method from this instance
-    Character.new(@name, @race, @class)
   end
 
-  def get_name 
-    puts 'What is your name?'
-    self.name = gets.chomp
+  def stat_roll
+    Dice.stat_roll
   end
 
-  def get_race
-    puts 'what is your race?' 
-    self.race = gets.chomp 
-  end
-
-  def get_class
-    puts 'what is your class?'
-    self.class = gets.chomp
+  def start_stats
+    while @character.stats.values.any?(nil)
+      puts "Ready to roll?"
+      roll = stat_roll
+      puts "You rolled #{roll}!"
+      puts "What stat do you want to set for #{roll}?"
+      input = gets.strip.downcase.to_sym
+      @character.stats[input] = roll
+    end
   end
   
-  def self.roll_stats
-    @initial_stats = {
-      :hp => 100,
-      :str => Dice.stat_roll,
-      :dex => Dice.stat_roll,
-      :con => Dice.stat_roll,
-      :wis => Dice.stat_roll,
-      :int => Dice.stat_roll,
-      :cha => Dice.stat_roll
-    }
-  end
-
- # def add_class_stat_bonus
- #   self.initial_stats.map {|stat}  
- # end
-
 end
+
+#   def get_name 
+#     puts 'What is your name?'
+#     self.name = gets.chomp
+#   end
+
+#   def get_race
+#     puts 'what is your race?' 
+#     self.race = gets.chomp 
+#   end
+
+#   def get_class
+#     puts 'what is your class?'
+#     self.class = gets.chomp
+#   end
+  
+#   def self.roll_stats
+#     @initial_stats = {
+#       :hp => 100,
+#       :str => Dice.stat_roll,
+#       :dex => Dice.stat_roll,
+#       :con => Dice.stat_roll,
+#       :wis => Dice.stat_roll,
+#       :int => Dice.stat_roll,
+#       :cha => Dice.stat_roll
+#     }
+#   end
+
+#  # def add_class_stat_bonus
+#  #   self.initial_stats.map {|stat}  
+#  # end
---
+++ spec/create_spec.rb
@@ -0,0 +1,32 @@
+describe CreateCharater do
+  
+  describe "builds character" do
+    
+    it "prompts user to set a name" do
+      
+    end
+    it "prompts to set klass" do
+      
+    end
+    it "prompts to set race" do
+      
+    end
+
+    it "rolls for stat" do
+      
+    end
+  end
+  describe "#start_stats" do
+    describe "#roll_for_stat" do    
+      it "rolls for one stat" do
+        expect()
+      end
+    end
+    describe "#set_stat" do
+      it "can assign to one stat at a time" do
+      end
+    end
+
+  end
+
+end
